e4a579a42abc1df15369343feaeaf725736c8000
thelittlemermaid <arielinman98@gmail.com>
2019-03-20 17:52:21 -0500

    deck is created and shuffled

--- Card.py
+++ Card.py
@@ -1,74 +1,90 @@
 import sys
+import random
 
 class Card:
-    rank = 0
-    suits = 0
-    number = 0
-    #Card.next()
-    cardSuit = ""
-    cardRank = ""
+    # rank = 0
+    # suits = 0
+    # number = 0
+    # Card.next()
+    # cardSuit = ""
+    # cardRank = ""
 
-    def __init__(self, val):
-            self.val = data
-            self.next = None
-            self.prev = None
+    # def __init__(self, val):
+    #         self.val = data
+    #         self.next = None
+    #         self.prev = None
 
-    def Card(self, rank, suits, number, temp):
+    def __init__(self, rank, suits, number, temp, *args, **kwargs):
         self._rank = rank
         self._suits = suits
         self._number = number
         self._temp = temp
+    
+    # def __init__(self, rank, suits):
+    #     self.rank = rank
+    #     self.suits = suits
 
-    def set_rank(self, rank):
-        self._rank = rank
+    # def set_rank(self, rank):
+    #     self._rank = rank
 
-    def set_suits(self, suits):
-        self._suits = suits
+    # def set_suits(self, suits):
+    #     self._suits = suits
       
-    def set_number(self, number):
-        self._number = number
+    # def set_number(self, number):
+    #     self._number = number
 
-    def set_cardSuit(self, cardSuit):
+    def cardSuit(self, cardSuit):
         self._cardSuit = cardSuit
+        return cardSuit
 
-    def set_cardRank(self, cardRank):
+    def cardRank(self, cardRank):
         self._cardRank = cardRank
+        return cardRank
 
-    def get_rank(self):
-        return self._rank
+    # def get_rank(self):
+    #     return self._rank
 
-    def get_suit(self):
-        return self._suits
+    # def get_suit(self):
+    #     return self._suits
 
-    def get_number(self):
-        return self._number
+    # def get_number(self):
+    #     return self._number
 
-    def get_cardSuit(self):
-        return self._cardSuit
+    # def get_cardSuit(self):
+    #     return self._cardSuit
 
-    def get_cardRank(self):
-        return self._cardRank
+    # def get_cardRank(self):
+    #     return self._cardRank
 
 class CardDeck:
-    deck = []
+    #deck = []
     rank = 0
     suits = 0
-    counter = 0
-
-    def suits(suits):
-        suitList = ["Spades", "Hearts", "Clubs", "Diamonds"]
-        return suitList
-
-    def rank(rank):
-        rankList = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    #counter = 0
 
     def cardDeck():
+        suitList = ["Spades", "Hearts", "Clubs", "Diamonds"]
+        rankList = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        deck = []
         for rankIndex in range (0, 13):
             for suitsIndex in range (0, 4):
-                 
+                newCard = Card(rankIndex, suitsIndex, number = 0, temp = 0)
+                deck.append((newCard.cardRank(rankList[rankIndex])) + " of " + (newCard.cardSuit(suitList[suitsIndex])))
+        return deck
+
+    def shuffle(deck):
+        tempCard = Card(rank = None, suits = None, number = None, temp = None)
+        for card in deck:
+            randomNum = random.randint(0, 51)
+            tempCard = card
+            card = randomNum
+            deck[randomNum] = tempCard
+        return deck
 
 def main():
-    CardDeck.cardDeck()
+    newCardDeck = CardDeck.cardDeck()
+    shuffledCardDeck = CardDeck.shuffle(newCardDeck)
+    print(shuffledCardDeck)
 
 if __name__ == "__main__":
     main()
