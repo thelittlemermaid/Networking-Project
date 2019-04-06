22f69fc117ebbbd905be063b1774d28fe29e559b
thelittlemermaid <arielinman98@gmail.com>
2019-04-06 15:18:15 -0500

    added main

--- Client.py
+++ Client.py
@@ -63,9 +63,12 @@ def main():
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientSocket.bind(('127.0.0.1', 8080))
     clientSocket.listen(2)
-    while True:
-        connect, serverAddress = socket.accept()
-        data = connect.recv(1024)
-        hand = pickle.loads(data)
+    connect, serverAddress = socket.accept()
+    print("Connected to:", serverAddress)
+    data = connect.recv(1024)
+    hand = pickle.loads(data)
     print(hand)
     clientSocket.close()
+
+if __name__ == "__main__":
+    main()
