5acc497b1d33c817cedc2f3afa80018c87b8d198
Admin <admin@Alexs-MacBook-Pro.local>
2019-04-15 18:07:55 -0500

    Added Documentation

--- Server.py
+++ Server.py
@@ -1,23 +1,52 @@
+"""
+War Card Game Server by: Ariel Inman & Alex Franklin
+Because Suits do not matter in War, we are only caring about the value of each card
+In the client we assign each card a RankValue from 0-12. (2-A).
+Here is the list of how the RankValues correspond to each card:
+2 - 0
+3 - 1
+4 - 2 
+5 - 3
+6 - 4
+7 - 5
+8 - 6
+9 - 7
+10 - 8
+Jack - 9
+Queen - 10
+King - 11
+Ace - 12
+When the values are recieved from the Client, we print what the RankValue is for each card for easy comparison
+"""
+
 import socket
 import ast
 
 response = 0
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 8080))
-server_socket.listen(30)
+#Enter IP for current machine
+server_socket.bind(('', 8081))
+server_socket.listen(5)
 conn, address = server_socket.accept()
 
 while True:
+    #Prints once connection is made and Server recieves first card
     print("Server Listeningngngngng")
+    #The Values are a list from the array of the two cards that are sent
     values = conn.recv(8192).decode()
+    #Converted from a string, back to a list
     convertedValues = ast.literal_eval(values)
     print('Data Recieved:', convertedValues)
 
+    #Assign player card which is first
     playerValue = convertedValues[0]
+    #Assign Server's Card which is second
     serverValue = convertedValues[1]
 
-    print("Server:", serverValue)
+    #Displayed for debugging purposes to make sure that the correct card is sent
+    #See above documentatiion for which values correspond to which cards
     print("Player:", playerValue)
+    print("Server:", serverValue)
 
     if playerValue > serverValue:
         response = 1
@@ -26,5 +55,6 @@ while True:
     elif playerValue == serverValue:
         response = 3
 
-    print(response)
+    #Response will be sent back to Client to finish appending cards to winner's hand
+    print("Response: ", response)
     conn.sendall(str(response).encode())
